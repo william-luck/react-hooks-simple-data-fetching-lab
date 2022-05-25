adf8b85671b86fe6594b816e44f4943ccd62a66c
William Luck <luck.william.h@gmail.com>
2022-05-25 15:11:00 -0500

    commit tree 256e1198fb70207e4eb1fddf0c95681be162d346

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import react, { useState, useEffect} from "react";
+
+function App() {
+    const [randomImage, setRandomImage] = useState(''); // set up state to store random image obtained from endpoint
+    const [isLoaded, setIsLoaded] = useState(false) // Used for conditional rendering of loading...
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(r => r.json())
+            .then(src => {
+                setRandomImage(src.message) //changes state, rerenders
+                setIsLoaded(true) // will take the loading off the page.
+            })
+
+    }, []); // Have to use the blank dependency array because it infinetely loaded,  because our side effect changes state, causes App to render, side effect kicks in again, etc. 
+
+    if (!isLoaded) return <p>Loading...</p> // WIll display before dog picture is loaded
+
+    return <img src={randomImage} alt="A Random Dog" /> // Final thing to return.
+
+    
+
+}
+
+export default App;
